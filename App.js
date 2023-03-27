a3aa7ed7155002f33c3f785608624243adf3bb64
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-27 08:57:34 -0600

    add public folder and stylesheet

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.set('view', path.join(__dirname, 'views'));
+
+app.set('view enginge', 'ejs');
+
+//le digo a express que use esto cuando se lo pido
+app.use(express.static('public'))
+
 app.get('/user/:id/:username', (req, res) => {
     let userId = req.params.id;
     let user = req.params.username;
